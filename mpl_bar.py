8b888b37b2aae9cc53863bf7e3811656d1799968
Regis066 <mugisharegis72@gmail.com>
2023-10-05 19:16:56 +0200

    csv file loading with pandas

--- mpl_bar.py
+++ mpl_bar.py
@@ -1,28 +1,44 @@
 from matplotlib import pyplot as plt
 import numpy as np 
+from collections import Counter
+import csv
+import pandas as pd
 # print(plt.style.available)
 plt.style.use('fivethirtyeight')
 # plt.xkcd()
+language_counter = Counter()
 
-age_x = [25,26,27,28,29,30,31,32,32,33,34]
-x_indexes = np.arange(len(age_x))
-width = 0.25 
-dev_y = [184960, 220000, 267520, 393200 , 432000, 560000, 62316, 34928, 273117, 18748, 13752]
+# with open('data.csv') as csv_file:
+#     csv_reader = csv.DictReader(csv_file)
 
-plt.bar(x_indexes - width, dev_y, color='#444444' , linestyle= '--',  label= "All Dev" , width=width)
-
-py_dev_y = [42300, 46852, 49320 , 54200, 58000, 62516, 64828, 675117, 68948, 73952, 80000]
-plt.bar(x_indexes , py_dev_y, label = "Python Dev", width=width)
-
-js_dev_y = [37810, 43515, 46823, 49293, 53437,56373,62375, 66674, 68745, 68746, 74583]
-plt.bar(x_indexes + width , js_dev_y , label = "JS Dev", width=width)
+data = pd.read_csv('data.csv')
+ids = data['Responder_id']
+languages_responses = data['LanguagesWorkedWith']
 
 
-plt.legend()
-plt.xticks(ticks=x_indexes , label=age_x)
-plt.xlabel("Age")
-plt.ylabel("Median Salary (USD)")
-plt.title('Median Salary (USD) by Age')
-plt.grid(True)
+for response in languages_responses:
+    language_counter.update(response.split(';'))
+
+# print(language_counter.most_common(15))
+
+languages = []
+popularity = []
+
+for item in language_counter.most_common(15):
+    languages.append(item[0])
+    popularity.append(item[1])
+
+languages.reverse()
+popularity.reverse()
+
+plt.barh(languages , popularity)
+
+
+
+
+# plt.xlabel("Programming Languages")
+plt.title('Most Popular Languages')
+plt.xlabel("Popularity (number of users)")
+# plt.grid(True)
 plt.tight_layout()
 plt.show()
